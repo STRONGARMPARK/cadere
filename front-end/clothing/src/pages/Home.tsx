d03e190568d2f17bb6ce872c7c2c155f9f851305
Gavin <gavin@fogel.io>
2021-02-08 14:43:01 -0500

    fix?

--- front-end/clothing/src/pages/Home.tsx
+++ front-end/clothing/src/pages/Home.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useGetClothes } from '../api/useClothing';
 
 interface HomeProps {}
 
 export const Home: React.FC<HomeProps> = () => {
+
   return (
     <div>
       <p>Home</p>
